0ad007fcfc1345ec6f42cb9784f5bceb08ef0d1a
ShijianXu <shijian.xu@unibas.ch>
2026-04-14 15:56:45 +0200

    add more test

--- examples/llada/sample_cluster_guided.py
+++ examples/llada/sample_cluster_guided.py
@@ -53,7 +53,7 @@ print("=" * 80)
 messages = [
     [{"role": "user", "content": "Lily runs 12 km/h for 4 hours. How far in 8 hours?"}],
     [{"role": "user", "content": "Please write an educational python function."}],
-    [{"role": "user", "content": "What is the capital of France?"}],
+    [{"role": "user", "content": "What nationality of the company that published Adventure Cyclist Magazine?"}],
 ]
 
 inputs = tokenizer.apply_chat_template(
